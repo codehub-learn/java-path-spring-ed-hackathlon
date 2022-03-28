f6dd7021b17f10805adc2281ac56d2b9a8ccb5ad
Constantinos Giannacoulis <c.giannacoulis@codehub.gr>
2022-03-28 16:55:05 +0300

    Committed main application class.
    Signed-off-by: Constantinos Giannacoulis <c.giannacoulis@codehub.gr>

---
+++ src/main/java/gr/codelearn/spring/showcase/hackathlon/HackathlonApplication.java
@@ -0,0 +1,11 @@
+package gr.codelearn.spring.showcase.hackathlon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HackathlonApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(HackathlonApplication.class, args);
+	}
+}
