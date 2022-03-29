be5904a0d0a6fc17df1db885d7b39347de4f0339
Constantinos Giannacoulis <c.giannacoulis@codehub.gr>
2022-03-29 12:02:46 +0300

    Added a bootstrap class we will use to insert sample content.
    Signed-off-by: Constantinos Giannacoulis <c.giannacoulis@codehub.gr>

---
+++ src/main/java/gr/codelearn/spring/showcase/hackathon/bootstrap/SampleContentCreator.java
@@ -0,0 +1,12 @@
+package gr.codelearn.spring.showcase.hackathon.bootstrap;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+@Component
+public class SampleContentCreator implements CommandLineRunner {
+	@Override
+	public void run(final String... args) throws Exception {
+
+	}
+}
